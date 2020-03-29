8e3655aac89d0e18a8dd13729e52ebc3fc2ff1bc
Ashis Kumar Singh <49238464+ashiscs@users.noreply.github.com>
2020-03-29 19:51:22 +0530

    Create QUES_3.C

---
+++ QUES_3.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char s1[8],s2[8];
+    //clrscr();
+    
+    scanf("%s%s",s1,s2);
+    printf("char1=%c, char2=%c",s1[52-'0'],s2[*s2-0[s2]]+4);
+    getch();
+}
+-----------------------------------------------------------------------------------------------------------------------
+
+/* OUTPUT
+
+MORNING                                                                                                                                       
+EVENING                                                                                                                                       
+char1=I, char2=I 
+
+*/
